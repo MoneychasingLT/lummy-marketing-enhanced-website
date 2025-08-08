75ff6574900a030c6c1e5fc14028c68ae52a7c83
MoneychasingLT <tomisindoescommerce@gmail.com>
2025-08-08 12:45:28 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
             <Route path="/portfolio" element={<Portfolio />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
           </Routes>
         </main>
         <Footer />
